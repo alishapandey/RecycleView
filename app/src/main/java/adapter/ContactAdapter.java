eb3999c94644cc04e509cb6a34bdc712f0d27466
alishapandey <alishaparis07@gmail.com>
2019-04-19 14:28:44 +0545

    Recycleview2

--- app/src/main/java/adapter/ContactAdapter.java
+++ app/src/main/java/adapter/ContactAdapter.java
@@ -1,6 +1,7 @@
 package adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.e.recycleview.DetailsActivity;
+import com.e.recycleview.MainActivity;
 import com.e.recycleview.R;
 
 import java.util.List;
@@ -36,6 +39,22 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.Contacts
         contactsViewHolder.imgProfile.setImageResource(contacts.getImageID());
         contactsViewHolder.tvName.setText(contacts.getName());
         contactsViewHolder.tvPhone.setText(contacts.getPhoneNo());
+
+
+contactsViewHolder.imgProfile.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(mContext, DetailsActivity.class);
+
+        intent.putExtra("image",contacts.getImageID());
+        intent.putExtra("name",contacts.getName());
+        intent.putExtra("phone",contacts.getPhoneNo());
+        intent.putExtra("email",contacts.getEmail());
+        intent.putExtra("address",contacts.getAddress());
+
+        mContext.startActivity(intent);
+    }
+});
 
     }
 
--- app/src/main/java/com/e/recycleview/MainActivity.java
+++ app/src/main/java/com/e/recycleview/MainActivity.java
@@ -2,6 +2,7 @@ package com.e.recycleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -21,14 +22,17 @@ public class MainActivity extends AppCompatActivity {
         recyclerView=findViewById(R.id.recyclerview);
 
         List<Contacts> contactsList = new ArrayList<>();
-        contactsList.add(new Contacts("Saughat Malla","9845050002",R.drawable.saugat));
-        contactsList.add(new Contacts("Rajesh Hamal","98425710000",R.drawable.rajesh));
-        contactsList.add(new Contacts("Dahayang Rai","9852462423",R.drawable.dahayang));
-        contactsList.add(new Contacts("Bhuwan KC","98400001212",R.drawable.bhuwan));
+        contactsList.add(new Contacts("Saughat Malla","9845050002","Saughat12@gmail.com","Kathmandu",R.drawable.saugat));
+        contactsList.add(new Contacts("Rajesh Hamal","98425710000","Rajesh12@gmail.com","Bhairahawa",R.drawable.rajesh));
+        contactsList.add(new Contacts("Dahayang Rai","9852462423","Dahayang12@gmail.com","Dharan",R.drawable.dahayang));
+        contactsList.add(new Contacts("Bhuwan KC","98400001212","Bhuwankc12@gmail.com","Kathmandu",R.drawable.bhuwan));
+        contactsList.add(new Contacts("Leo Messi","98400001212","Leomessi12@gmail.com","Argentina",R.drawable.leo));
+        contactsList.add(new Contacts("Zayn Malik","98400001212","Zaynmalik12@gmail.com","California",R.drawable.zayn));
+        contactsList.add(new Contacts("Salman khan","98400001212","Salmankhan12@gmail.com","Mumbai",R.drawable.salman));
 
         ContactAdapter contactsAdapter= new ContactAdapter(this,contactsList);
         recyclerView.setAdapter(contactsAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
 
     }
